e6e4d89415549f6503ab224e0e30b8d6a54d0e70
Miguel E. Cabrera <mecg1994@gmail.com>
2026-07-31 09:15:56 -0600

    cambos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './layout/navbar/navbar.component';
-import { FooterComponent } from './layout/footer/footer.component';
-import { HomeComponent } from './pages/home/home.component';
-import { FeaturesComponent } from './pages/features/features.component';
-import { MobileComponent } from './pages/mobile/mobile.component';
-import { PricingComponent } from './pages/pricing/pricing.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { LiveChatModule } from './components/live-chat/live-chat.module';
+
+// Componentes de la landing
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { MobileComponent } from './components/mobile/mobile.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { ContactComponent } from './components/contact/contact.component';
+
+// 
+import { AdvantagesComponent } from './components/advantages/advantages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,    // ← AGREGAR
-    FooterComponent,    // ← AGREGAR
+    NavbarComponent,
+    FooterComponent,
     HomeComponent,
     FeaturesComponent,
     MobileComponent,
     PricingComponent,
-    ContactComponent
+    ContactComponent,
+    AdvantagesComponent   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LiveChatModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
